0dbb484e56004591d6235ed1e2f9d4b9f6375719
anikaaboni <39457741+anikaaboni@users.noreply.github.com>
2018-06-11 21:31:01 +0600

    Add files via upload

---
+++ 16.02.04.072_OFFLINE_1_STL.cpp
@@ -0,0 +1,51 @@
+#include<stdio.h>
+#include<vector>
+#include<iostream>
+
+using namespace std;
+int main()
+{
+    while(1)
+    {
+        int n,k;
+        cin>>n>>k;
+        int u,v,flag=0;
+
+        vector<int> arr;
+        for(int i=1;i<=n;i++)
+        {
+            int a;
+            cin>>a;
+            arr.push_back(a);
+        }
+
+        for(u=0;u<n-1;u++)
+        {
+            for(v=u+1;v<n;v++)
+            {
+                if(arr[u] + arr[v] == k)
+                {
+                    flag=1;
+                    break;
+
+                }
+
+            }
+        }
+
+        if(flag == 1)
+        {
+            cout<<"YES"<<endl;
+        }
+        else
+        {
+            cout<<"NO"<<endl;
+        }
+
+
+
+    }
+    return 0;
+
+
+}
